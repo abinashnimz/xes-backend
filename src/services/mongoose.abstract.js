6a86bb9ea88b6731d86af0cce3498c57798f5955
abinashnimz <abinash443steam@gmail.com>
2025-10-24 01:03:55 +0530

    added all neceeessary modular mongoose abstracts

--- src/services/mongoose.abstract.js
+++ src/services/mongoose.abstract.js
@@ -1,31 +1,50 @@
 export default DOCUMENT_MODEL => ({
-	create: data => new Promise(
-		(resolve, reject) => DOCUMENT_MODEL.create(data, (err, data) => err ? reject(err) : resolve(data))
-	),
-	findOne: (criteria, project) => new Promise(
-		(resolve, reject) => DOCUMENT_MODEL.findOne(criteria, project).lean().exec(
-			(err, data) => err ? reject(err) : resolve(data)
-		)
-	),
-	findAll: (criteria, project, sort, skip, limit) => new Promise(
-		(resolve, reject) => DOCUMENT_MODEL.find(criteria, project, { sort, skip, limit}).lean().exec(
-			(err, data) => err ? reject(err) : resolve(data)
-		)
-	),
-	updateOne: (criteria, data) => new Promise(
-		(resolve, reject) => DOCUMENT_MODEL.findByIdAndUpdate(criteria, data, { returnDocument: 'after' }).lean().exec(
-			(err, data) => err ? reject(err) : resolve(data)
-		)
-	),
-	deleteOne: (id) => new Promise(
-		(resolve, reject) => DOCUMENT_MODEL.findByIdAndDelete(id, (err, data) => err ? reject(err) : resolve(data))
-	),
-	count: () => new Promise(
-		(resolve, reject) => DOCUMENT_MODEL.estimatedDocumentCount((err, data) => err ? reject(err) : resolve(data))
-	),
-	countDocuments: criteria => new Promise(
-		(resolve, reject) => DOCUMENT_MODEL.countDocuments(criteria).exec(
-			(err, data) => err ? reject(err) : resolve(data)
-		)
-	)
+    create: async (data)=>{
+    try{
+        return await DOCUMENT_MODEL.create(data);
+    }catch(err){
+        throw err;
+    }},
+    findOne: async (criteria, project)=>{
+        try{
+            return await DOCUMENT_MODEL.findOne(criteria, project).lean().exec();
+        }catch(err){
+            throw err;
+        }
+    },
+    findAll: async (criteria, project, sort, skip, limit)=>{
+        try{
+            return await DOCUMENT_MODEL.find(criteria, project, {sort, skip, limit}).lean().exec();
+        }catch(err){
+            throw err
+        }
+    },
+    updateOne: async (criteria, data)=>{
+        try{
+            return await DOCUMENT_MODEL.findByIdAndUpdate(criteria, data, {returnDocument: 'after'}).lean().exec();
+        }catch(err){
+            throw err
+        }
+    },
+    deleteOne: async (id)=>{
+        try{
+            return await DOCUMENT_MODEL.findByIdAndDelete(id);
+        }catch(err){
+            throw err
+        }
+    },
+    count: async ()=>{
+        try{
+            return await DOCUMENT_MODEL.estimatedDocumentCount();
+        }catch(err){
+            throw err
+        }
+    },
+	countDocuments: async (criteria) =>{
+		try{
+			return await DOCUMENT_MODEL.countDocuments(criteria).exec();
+		}catch(err){
+			throw err;
+		}
+	}
 })
