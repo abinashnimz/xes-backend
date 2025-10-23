18641712ff882c131764d0b6bab42c4c2710c340
abinashnimz <abinash443steam@gmail.com>
2025-10-24 01:12:29 +0530

    minor bug fix

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -4,8 +4,8 @@ import constant from '../constant.js';
 export default async () => {
     try{
         await mongoose.connect(constant.MONGODB_URL);
-        console.log("✅ Database connection successful");
+        console.log("Database connection successful ✅");
     }catch(err){
-        console.log("❌ Database connection failed:", err);
+        console.log("Database connection failed ❌", err);
     }
 }
