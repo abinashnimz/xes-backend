b9b69b022d9d278e088a4ce12a783a3f90530721
abinashnimz <abinash443steam@gmail.com>
2025-10-24 23:34:05 +0530

    jwt token added to the auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,9 @@
+import constant from '../constant.js';
 import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
+
+//Password Services
 export const hashPasswordBcrypt = async password => {
   try {
     return await bcrypt.hash(password, 12);
@@ -15,3 +19,33 @@ export const comparePasswordBcrypt = async (password, hash) =>{
         throw err;
     }
 }
+
+
+//JWT Auth Token Services
+//Split token & Bearer from req-headers
+const getToken = authHeader => authHeader.split(' ')[1];
+
+// generate JWT token with user _id as data
+const generateJwtToken = SECRET => ({ _id, role }) => jwt.sign({ _id, role }, SECRET);
+
+
+const verifyJwtToken = SECRET => (req, res, next) => {
+	const authHeader = req.headers.authorization
+	if (!authHeader) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'invalid-user' })
+	const token = getToken(authHeader)
+	jwt.verify(token, SECRET, (err, data) => {
+		if (err) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'token-expired' })
+		if (!data?._id) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'invalid-user' })
+		req['user'] = data
+		next()
+	})
+}
+
+
+
+
+
+
+
+const useJwt = SECRET => [generateJwtToken(SECRET), verifyJwtToken(SECRET)];
+export const [generateAuthToken, verifyAuthToken] = useJwt(constant.JWT_SECRET)
