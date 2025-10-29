fd687d871028143c5c14a6eaf82cc5128132998f
abinashnimz <abinash443steam@gmail.com>
2025-10-29 21:40:05 +0530

    minor bug fix in constant file

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 'use strict'
 export default Object.freeze({
-	MONGODB_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/xesports-api-services-test',
+	MONGODB_URL: process.env.MONGODB_URI_DEV || 'mongodb://127.0.0.1:27017/xesports-api-services-test',
 	APP_DEBUG: process.env.APP_DEBUG === 'true',
 	PORT: process.env.PORT || 3009,
 	RESPONSE: {
