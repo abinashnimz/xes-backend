cddbd7934c47fba6fa850ae43dbbe1556ff4830a
abinashnimz <abinash443steam@gmail.com>
2025-10-31 01:48:23 +0530

    email verification jwt token added to the auth services

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -28,17 +28,32 @@ const getToken = authHeader => authHeader.split(' ')[1];
 // generate JWT token with user _id as data
 const generateJwtToken = SECRET => ({ _id, role }) => jwt.sign({ _id, role }, SECRET);
 
-
 const verifyJwtToken = SECRET => (req, res, next) => {
-	const authHeader = req.headers.authorization
-	if (!authHeader) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'invalid-user' })
-	const token = getToken(authHeader)
+	const authHeader = req.headers.authorization;
+	if (!authHeader) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'invalid-user' });
+	const token = getToken(authHeader);
 	jwt.verify(token, SECRET, (err, data) => {
-		if (err) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'token-expired' })
-		if (!data?._id) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'invalid-user' })
-		req['user'] = data
-		next()
+		if (err) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'token-expired' });
+		if (!data?._id) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json({ message: 'invalid-user' });
+		req['user'] = data;
+		next();
 	})
+}
+
+
+// Email Verification Token and vierify token 
+const generateJwtTokenEmailVerification = SECRET => ({ _id }) => jwt.sign({ userId: _id }, SECRET, { expiresIn: '1d' });
+
+const verifyJwtTokenEmailVerification = SECRET => (req, res, next) => {
+	const authHeader = req.headers.authorization
+	if (!authHeader) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json('No JWT token provided');
+	const token = getToken(authHeader);
+	jwt.verify(token, SECRET, (err, data) => {
+		if (err) return res.sendStatus(constant.RESPONSE.UNAUTHORIZED.STATUS);
+		if (!data?.userId) return res.status(constant.RESPONSE.UNAUTHORIZED.STATUS).json('No user id found in token');
+		req['user'] = data;
+		next();
+	});
 }
 
 
@@ -46,6 +61,10 @@ const verifyJwtToken = SECRET => (req, res, next) => {
 
 
 
-
+//JWT Authentication Service
 const useJwt = SECRET => [generateJwtToken(SECRET), verifyJwtToken(SECRET)];
-export const [generateAuthToken, verifyAuthToken] = useJwt(constant.JWT_SECRET)
+export const [generateAuthToken, verifyAuthToken] = useJwt(constant.JWT_SECRET);
+
+//Email Verification Auth Service
+const useEmailJwt = SECRET => [generateJwtTokenEmailVerification(SECRET), verifyJwtTokenEmailVerification(SECRET)];
+export const [getEmailVerificationToken, verifyEmailVerificationToken] = useEmailJwt(constant.JWT_SECRET);
